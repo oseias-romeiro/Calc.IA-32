d8eb429915dc30608620836df4b7da8c35355d48
oseias-romeiro <oseiasromeirom@gmail.com>
2023-06-30 23:01:31 -0300

    print

--- src/calculator.asm
+++ src/calculator.asm
@@ -1,12 +1,31 @@
 section .data
+    msg1        db "Bem-vindo. Digite seu nome:", 0
+    msg1_size   EQU $-msg1
+
 
 section .text
     global _start
 
 
 _start:
-    
+    push msg1_size
+    push msg1
+    call print
+    add  esp, 8
+
     ; exit
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
+
+print:
+    ; args: len, msg
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, [esp+4]
+    mov edx, [esp+8]
+    int 80h
+
+    ret
+
